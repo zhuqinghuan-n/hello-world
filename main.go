4d36433108b5e27e104e510756fa4a81646a7665
zhuqinghuan-n <76102998+zhuqinghuan-n@users.noreply.github.com>
2021-01-11 20:46:34 +0800

    Create main.go

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	for i := 1; i < 10; i++ {
+		for j := 1; i >= j && j < 10; j++ {
+			fmt.Printf("%d*%d=%d\t", i, j, i*j)
+		}
+		fmt.Println()
+	}
+}
